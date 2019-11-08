e894ac2f6b31e3c8c74eae8b7bde6d269f21b16b
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-08 15:30:12 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
+int main(){
+  int fd1 = open("/dev/random", O_RDONLY);
+  int fd2 = open("file.txt", O_RDWR | O_CREAT, 0644);
+  return 0;
