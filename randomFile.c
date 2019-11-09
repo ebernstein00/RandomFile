40cb14ab6a1d773dd200dbfe60deca28ac2617a2
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-09 14:39:33 -0500

    Update randomFile.c

--- randomFile.c
+++ randomFile.c
@@ -2,9 +2,26 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 
+int randNumGen(){
+	int fd = open("/dev/random");
+	int bytes = read(fd, int stor[1], sizeof(int));
+	return bytes;
+	close(fd);
+}
+
 int main(){
-  int fd1 = open("/dev/random", O_RDONLY);
-  int fd2 = open("file.txt", O_RDWR | O_CREAT, 0644);
-  return 0;
+	//Create int array
+	int arr[10];
+	int i;
+	for(i = 0; i < 10; i++){
+		arr[i] = randNumGen();
+	}
+
+	//Print array values...
+	for(i = 0; i < 10; i++){
+		printf("Index %d:\t%d", i, arr[i]);
+	}
+}
