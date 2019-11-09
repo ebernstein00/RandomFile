d9df960856269b3f50dcd44d1513c7c11944d498
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-09 14:55:36 -0500

    Update randomFile.c

--- randomFile.c
+++ randomFile.c
@@ -8,13 +8,13 @@
 int randNumGen(){
 	int fd = open("/dev/random", O_RDONLY);
 	int buf[1];
-	int bytes = read(fd, buf, sizeof(int));
+	read(fd, buf, sizeof(int));
 	return buf[0];
 	close(fd);
 }
 
 int main(){
-	//Create int array
+	//Create int array...
 	int arr[10];
 	int i;
 	for(i = 0; i < 10; i++){
@@ -24,5 +24,23 @@ int main(){
 	//Print array values...
 	for(i = 0; i < 10; i++){
 		printf("Index %d:\t%d\n", i, arr[i]);
+	}
+
+	//Write to another file...
+	int wrtr = open("file.txt", O_WRONLY);
+	write(wrtr, arr, sizeof(int));
+	printf("Data written to file...");
+	close(wrtr);
+
+	//Reading into new array...
+	int veri[10];
+	int rdr = open("file.txt", O_RDONLY);
+	read(rdr, veri, sizeof(int));
+	close(rdr);
+
+	//Print new array...
+	printf("Verification:\n")
+	for(i = 0; i < 10; i++){
+		printf("Index %d:\t%d\n", i, veri[i]);
 	}
 }
